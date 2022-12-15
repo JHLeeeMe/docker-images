b348a729380b8a976fffeec10f77267092e0b8bb
JHLeeeMe <lejung92@gmail.com>
2022-12-15 16:07:45 +0900

    [Update] typo & docker network 추가
    	수정함:        zeppelin/0.8.2/config/zeppelin/zeppelin-env.sh
    	수정함:        zeppelin/0.8.2/run-container.sh

--- zeppelin/0.8.2/config/zeppelin/zeppelin-env.sh
+++ zeppelin/0.8.2/config/zeppelin/zeppelin-env.sh
@@ -3,7 +3,7 @@ export JAVA_HOME=/opt/java
 # for yarn mode
 export SPARK_HOME=/opt/spark
 export HADOOP_CONF_DIR=/opt/hadoop/etc/hadoop
-export MASTER=yarn-cluster
+export MASTER=yarn
 
 # for pyspark
 export PYSPARK_PYTHON=/usr/bin/python3
--- zeppelin/0.8.2/run-container.sh
+++ zeppelin/0.8.2/run-container.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
 
+# Create network
+line=$(docker network ls |grep zeppelin_net |wc -l)
+if [ $line -eq 0 ]; then
+    docker network create \
+        --subnet 10.0.0.0/24 \
+        zeppelin_net
+fi
+
 docker run \
     -itd \
-    --network spark-cluster \
     --name zeppelin \
+    --hostname zeppelin \
+    --network zeppelin_net \
     --ip 10.0.0.20 \
     -p 7777:7777 \
     jhleeeme/zeppelin:0.8.2 \
